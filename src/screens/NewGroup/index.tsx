c704535f62b3c8c3df7ef204e22a74266b097e92
jose cassio <josecassio2013@gmail.com>
2024-04-22 21:11:33 -0300

    feat: create input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@components/Header";
 import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -16,7 +17,13 @@ export function NewGroup() {
           subTitle="Crie uma nova turma para adicionar as pessoas"
         />
 
-        <Button title="Criar turma" onPress={() => {}} />
+        <Input placeholder="Nome da turma" />
+
+        <Button
+          title="Criar turma"
+          onPress={() => {}}
+          style={{ marginTop: 20 }}
+        />
       </Content>
     </Container>
   );
